e2c5e759e2cb40bff66fd169c2b3e03ca08fb9df
ajiang-boxlabs <ajiang@boxlabs.ca>
2020-12-20 21:28:19 -0500

    working api in search

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,146 @@
 <template>
   <v-app>
-    <v-autocomplete
-      v-model="select"
-      :loading="loading"
-      :items="items"
-      :search-input.sync="search"
-      cache-items
-      class="mx-4"
-      flat
-      hide-no-data
-      hide-details
-      label="What state are you from?"
-      solo-inverted
-    ></v-autocomplete>
+    <div class="appStyle">
+      <v-row class="centered">
+        <h1>The Shoppies</h1>
+      </v-row>
+      <v-row class="centered pt-10">
+        <v-autocomplete
+          v-model="searchResult"
+          :items="items"
+          :loading="isLoading"
+          :search-input.sync="search"
+          @change="itemSelect"
+          clearable
+          hide-details
+          item-text="Title"
+          item-value="Title"
+          label="Search for a movie..."
+          solo
+        >
+          <template v-slot:item="{ item }">
+            <v-avatar left>
+              <img :src="item.Poster" alt="Movie" />
+            </v-avatar>
+
+            <v-list-item-content class="ml-4">
+              <v-list-item-title v-text="item.Title"></v-list-item-title>
+              <v-list-item-subtitle v-text="item.Year"></v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-icon v-if="item.Type === 'game'">mdi-gamepad-variant</v-icon>
+              <v-icon v-if="item.Type === 'movie'">mdi-movie</v-icon>
+              <v-icon v-if="item.Type === 'episode'"
+                >mdi-television-classic</v-icon
+              >
+              <v-icon v-if="item.Type === 'series'"
+                >mdi-television-classic</v-icon
+              >
+            </v-list-item-action>
+          </template>
+        </v-autocomplete>
+      </v-row>
+
+      <v-row class="pt-10">
+        <v-col>
+          <h2>My Selected Nominations</h2>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" class="pr-0">
+          <MovieCard />
+        </v-col>
+        <v-col cols="12" class="pr-0">
+          <MovieCard />
+        </v-col>
+        <v-col cols="12" class="pr-0">
+          <MovieCard />
+        </v-col>
+        <v-col cols="12" class="pr-0">
+          <MovieCard />
+        </v-col>
+      </v-row>
+    </div>
   </v-app>
 </template>
 
 <script>
+import MovieCard from "./components/MovieCard";
 export default {
   name: "App",
-
-  components: {},
-
+  components: { MovieCard },
   data: () => ({
-    //
+    apiKey: "464910c",
+    isLoading: false,
+    awaitingSearch: false,
+    selectedItems: [],
+    items: [],
+    searchResult: null,
+    search: "",
+    value: null,
+    tab: null,
   }),
+  methods: {
+    itemSelect() {
+      this.$nextTick(() => {
+        this.search = "";
+        this.searchResult = null;
+      });
+    },
+  },
+  watch: {
+    search(val) {
+      if (!val) return;
+      if (val.length < 3) return;
+      this.value = val;
+      if (!this.awaitingSearch) {
+        setTimeout(() => {
+          this.isLoading = true;
+          const concatVal = this.value.split(" ").join("+");
+
+          fetch(`https://omdbapi.com/?s=${concatVal}&apikey=${this.apiKey}`)
+            .then((res) => res.clone().json())
+            .then((res) => {
+              if (res.Response) {
+                this.items = res.Search;
+                console.log(this.items);
+              }
+            })
+            .catch((err) => {
+              console.log("ERROR", err);
+            })
+            .finally(() => {
+              this.isLoading = false;
+              this.awaitingSearch = false;
+            });
+        }, 1000); // 1 sec delay
+        this.awaitingSearch = true;
+      }
+    },
+  },
 };
 </script>
+
+<style>
+.appStyle {
+  padding: 10% 20% 0 20%;
+}
+
+.centered {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.right {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+.left {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+</style>
---
+++ src/components/MovieCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card class="cardStyle">
+    <v-row>
+      <v-img
+        v-if="$vuetify.breakpoint.smAndUp"
+        class="imageStyle"
+        @click="$router.push(name)"
+        transition="fade"
+        max-width="75"
+        src="https://m.media-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg"
+      />
+
+      <v-col pl-0>
+        <v-card-title class="pb-0">PoopooPeepeeCheck </v-card-title>
+        <v-card-text class="pt-0">PoopooPeepeeCheck</v-card-text>
+      </v-col>
+      <v-col :style="{ maxWidth: '48px' }" class="right pr-8">
+        <v-icon>mdi-delete </v-icon>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "MovieCard",
+};
+</script>
+
+<style>
+.cardStyle {
+  border-radius: 12px !important;
+}
+.imageStyle {
+  border-top-left-radius: 12px;
+  border-bottom-left-radius: 12px;
+}
+
+@media all and (max-width: 400px) {
+  .v-card__title {
+    font-size: 14px !important;
+  }
+
+  .v-card__text {
+    font-size: 12px !important;
+  }
+}
+</style>
